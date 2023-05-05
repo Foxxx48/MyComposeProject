10b16f7f929f946bd97731a6d4c9e2b7d1fe9b7a
Foxxx48 <Foxxdim@mail.ru>
2023-05-05 12:09:13 +0300

    Lesson2 Raw, Box, Modifier

---
+++ app/src/main/java/com/example/mycomposeproject/Lesson2.kt
@@ -0,0 +1,57 @@
+package com.example.mycomposeproject
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class Lesson2 {
+
+    @Preview
+    @Composable
+    fun MultiplicationTable() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            for (i in 1 until 10) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                ) {
+                    for (j in 1 until 10) {
+
+                        val color =
+                        if ((i + j) % 2 == 0) {
+                            Color.Yellow
+                        } else {
+                            Color.White
+                        }
+                            Box(
+                                modifier = Modifier
+                                    .background(color)
+                                    .fillMaxSize()
+                                    .weight(1f)
+                                    .border(1.dp, Color.Black),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(text = "${i * j}")
+                            }
+                    }
+                }
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/mycomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeproject/MainActivity.kt
@@ -3,16 +3,25 @@ package com.example.mycomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val lesson2 = Lesson2()
         setContent {
-            HelloUser(name = "Dmitry", age = 39)
+            lesson2.MultiplicationTable()
         }
     }
 }
@@ -20,14 +29,27 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun HelloUserPreview() {
-    HelloUser(name = "Dmitry", age = 39)
+MyTable(s = 1, r = 10)
 }
 
 @Composable
-fun HelloUser(name: String, age: Int) {
-    Column {
-        repeat(10) {
-            Text(text = "Hello $name! You are ${age + it} years old.")
+fun MyTable(s: Int, r: Int) {
+    Column(
+        modifier = Modifier
+        .fillMaxSize()
+    ) {
+        for(i in s .. r) {
+            Box(
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxSize()
+                    .weight(1f)
+                    .border(1.dp, Color.Black),
+                contentAlignment = Alignment.Center
+            ){
+                Text(text = "$i")
+            }
+
         }
     }
 }
